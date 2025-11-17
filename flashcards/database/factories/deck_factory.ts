0f5329dff44d7ccf8bb85b4b20e8581e35a6b273
DevStackJunior <pv00wyv@eduvaud.ch>
2025-11-17 11:20:33 +0100

    feat : Create factories
    Init factories :
    - deck
    - flashcards
    
    Update properties match with :
    - deck models
    - flashcards models

--- flashcards/database/factories/deck_factory.ts
+++ flashcards/database/factories/deck_factory.ts
@@ -1,8 +1,11 @@
 import factory from '@adonisjs/lucid/factories'
 import Deck from '#models/deck'
+import { DateTime } from 'luxon'
 
 export const DeckFactory = factory
-  .define(Deck, async ({ faker }) => {
-    return {}
+  .define(Deck, () => {
+    return {
+      publishedDate: DateTime.now().minus({ days: Math.round(Math.random() * 365) }),
+    }
   })
   .build()
--- flashcards/database/factories/flashcard_factory.ts
+++ flashcards/database/factories/flashcard_factory.ts
@@ -1,8 +1,12 @@
 import factory from '@adonisjs/lucid/factories'
 import Flashcard from '#models/flashcard'
+import { DateTime } from 'luxon'
 
 export const FlashcardFactory = factory
-  .define(Flashcard, async ({ faker }) => {
-    return {}
+  .define(Flashcard, ({ faker }) => {
+    return {
+      question: faker.lorem.sentence(),
+      reponse: faker.lorem.sentence(),
+    }
   })
   .build()
